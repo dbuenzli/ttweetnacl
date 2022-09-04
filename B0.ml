250275cce955f8727a84d9af47615ef65a695304
Daniel Bünzli <daniel.buenzli@erratique.ch>
2022-09-04 22:57:45 +0200

    Track b0

--- B0.ml
+++ B0.ml
@@ -1,5 +1,4 @@
 open B0_kit.V000
-open B00_std
 open Result.Syntax
 
 (* OCaml library names *)
@@ -20,7 +19,7 @@ let test_exe file ~doc =
   let file = Fpath.v file in
   let srcs = [test_src file] in
   let requires = [ ttweetnacl ] in
-  B0_ocaml.exe (Fpath.basename ~no_ext:true file) ~doc ~srcs ~requires
+  B0_ocaml.exe (Fpath.basename ~strip_exts:true file) ~doc ~srcs ~requires
 
 let test = test_exe "test.ml" ~doc:"Ttweetnacl tests"
 
@@ -28,25 +27,25 @@ let test = test_exe "test.ml" ~doc:"Ttweetnacl tests"
 
 let default =
   let meta =
-    let open B0_meta in
-    empty
-    |> add authors ["The ttweetnacl programmers"]
-    |> add maintainers ["Daniel Bünzli <daniel.buenzl i@erratique.ch>"]
-    |> add homepage "https://erratique.ch/software/ttweetnacl"
-    |> add online_doc "https://erratique.ch/software/ttweetnacl/doc"
-    |> add licenses ["ISC"]
-    |> add repo "git+https://erratique.ch/repos/ttweetnacl.git"
-    |> add issues "https://github.com/dbuenzli/ttweetnacl/issues"
-    |> add description_tags ["cryptography"; "bindings"; "nacl";
-                             "org:erratique"; ]
-    |> add B0_opam.Meta.build
+    B0_meta.empty
+    |> B0_meta.(add authors) ["The ttweetnacl programmers"]
+    |> B0_meta.(add maintainers)
+       ["Daniel Bünzli <daniel.buenzl i@erratique.ch>"]
+    |> B0_meta.(add homepage) "https://erratique.ch/software/ttweetnacl"
+    |> B0_meta.(add online_doc) "https://erratique.ch/software/ttweetnacl/doc"
+    |> B0_meta.(add licenses) ["ISC"]
+    |> B0_meta.(add repo) "git+https://erratique.ch/repos/ttweetnacl.git"
+    |> B0_meta.(add issues) "https://github.com/dbuenzli/ttweetnacl/issues"
+    |> B0_meta.(add description_tags)
+      ["cryptography"; "bindings"; "nacl"; "org:erratique"; ]
+    |> B0_meta.tag B0_opam.tag
+    |> B0_meta.add B0_opam.build
       {|[["ocaml" "pkg/pkg.ml" "build" "--dev-pkg" "%{dev}%"]]|}
-    |> tag B0_opam.tag
-    |> add B0_opam.Meta.depends
+    |> B0_meta.add B0_opam.depends
       [ "ocaml", {|>= "4.13.0"|};
         "ocamlfind", {|build|};
         "ocamlbuild", {|build|};
          "topkg", {|build & >= "1.0.3"|}; ]
   in
-  B0_pack.v "default" ~doc:"ttweetnacl" ~meta ~locked:true @@
+  B0_pack.make "default" ~doc:"ttweetnacl" ~meta ~locked:true @@
   B0_unit.list ()
